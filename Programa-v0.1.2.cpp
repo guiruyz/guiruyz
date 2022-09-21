523af37ca0d04de37696a3ad7141da425c795b9e
Guilherme Ruyz <99899095+guiruyz@users.noreply.github.com>
2022-09-21 17:34:46 -0300

    Update version to 0.1.2

--- Programa-v0.1.2.cpp
+++ Programa-v0.1.2.cpp
@@ -1,6 +1,7 @@
 //Firmaware development by @guiruyz
 int pwm_out1 = 3;
 int transd_pin = A1;
+int var_pwm;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,10 +12,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   char *msg = (char *)malloc(16); 
+  var_pwm = 255; 
   while (1){
-  pinMode (pwm_out1 , OUTPUT);
-  analogWrite(pwm_out1, 255);
   int value = analogRead(transd_pin);
+  pinMode (pwm_out1 , OUTPUT);
+  if (value >= 600 ) {
+    var_pwm = 65;
+  }
+  analogWrite(pwm_out1, var_pwm);
   snprintf(msg, 16 , "Value: %d \n", value);
   Serial.print(msg);
   }
